28aeb39a0d2e4e843c862c0f42240f1a7a7e402c
DurgaMewada <mewadadurga83@gmail.com>
2024-05-03 12:24:53 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:timer_daily_task/DigitalClock.dart';
+import 'Clock.dart';
 import 'Types_of_Buttons.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: MyClock(),
+    home: Clock(),
   ));
 }
